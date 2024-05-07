56f0912417500b7e41bd058adeed6fb7f45741f8
xincmg <21194420+xincmg@users.noreply.github.com>
2024-05-07 20:19:05 +0800

    新增Hdfans

---
+++ sites/hdfans.py
@@ -0,0 +1,38 @@
+
+import requests
+from lxml import etree
+
+from sites.siteBase import SiteBase
+
+
+class Hdfans(SiteBase):
+
+    def login(self):
+        self.session.headers.update({
+            'Cookie': self.user.token,
+        })
+        return True
+
+    def signin(self):
+        url = 'https://hdfans.org/attendance.php'
+        try:
+            response = self.session.get(url, timeout=5)
+        except requests.RequestException as e:
+            self.state = str(e)
+            return None
+        return response
+
+    def report(self, response):
+        if response.text.find('href="logout.php">退出</a>]')>=0:
+            dom = etree.HTML(response.text)
+            etree.strip_tags(dom, 'tbody', 'b', 'p')
+            element = dom.xpath("//td[@class='embedded']/table/tr/td/text()")
+            self.state = ''.join(element).strip()
+        else:
+            self.state = '未登录'
+
+    def test(self):
+        self.login()
+        response = self.session.get(
+            'https://www.pthome.net/index.php', timeout=5)
+        print(response.text)
